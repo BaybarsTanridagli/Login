067c4a54f376e9bd34f8ad9add870bb755fe4d44
Baybars Bozok Tanridagli <baybarsstanridagli@gmail.com>
2023-06-09 23:27:12 +0300

    Login

---
+++ LoginSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "admin";
+        string pw = "1881";
+
+        Console.WriteLine("Please Enter Username");
+        string username = Console.ReadLine();
+        
+        if (username !=name)
+        {
+            Console.WriteLine("Only admin login allowed");
+        }
+        else
+        {
+            Console.WriteLine("Hello Admin , \nPlease Password:");
+            string password = Console.ReadLine();
+
+            if (password == pw)
+            {
+                Console.WriteLine("Congratulations Successful Login");
+            }
+            else
+            {
+                Console.WriteLine("You entered the wrong password");
+            }
+        }
+
+    }
+}
